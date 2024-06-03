20382c140b17382e3b0e0dac8d2de8b6c2fb86e5
edyerena086 <edyerena086@gmail.com>
2024-06-03 14:31:07 -0600

    Added graphql initial settings into the project

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,9 +1,21 @@
 import { Module } from '@nestjs/common';
+import { GraphQLModule } from '@nestjs/graphql'
+import { ApolloDriver, ApolloDriverConfig } from '@nestjs/apollo'
+
+// Import own module files
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { join } from 'path';
+import { PostModule } from './resources/post/post.module';
 
 @Module({
-  imports: [],
+  imports: [
+    GraphQLModule.forRoot<ApolloDriverConfig>({
+      driver: ApolloDriver,
+      autoSchemaFile: join(process.cwd(), 'src/graphql/schema.gql')
+    }),
+    PostModule
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
